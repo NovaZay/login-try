ceb4606051574b0648e2524719ee9bd636a74f98
NovaZay <novazaypersonal@gmail.com>
2025-05-31 16:17:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,56 @@
-#
-# I will update the code for the love of god stop asking 😭😭.
-# I'll make it better and better the more I learn, this is an updatable (does that even exist?) project.
-# Genuinely tired of comenting line for line while it not being useful at all so yes, summarizing!!
-#
-
-
-
-
-# Import some modules, for delays and opening links.
 import time
-import webbrowser
 
 
-# Set both set_pwd and input_pwd as blank variables so the can easily be changed later.
-set_pwd = ""
-input_pwd = ""
+try:
+    f = open("datos.txt", "x")
+except FileExistsError:
+    print("File already created, reading...")
 
-
-
-# My first function!!!! Probably not so necessary but, I'm happy. Its only purpose is learning probably, besides of the main function of being able to call 6 lines of code with only login()-
 def login():
-    print("Welcome back!")
-    time.sleep(0.6)
-    global set_pwd
-    print("Please input your password")
-    global input_pwd
-    input_pwd= input("Password: ")
+    username_input = input("User: ")
+    password_input = input("Password: ")
 
+    with open("datos.txt", "r") as f:
+        lines = f.readlines()
 
+    # Recorremos en pasos de 2: una línea es el usuario, la siguiente es la contraseña
+    for i in range(0, len(lines), 2):
+        user = lines[i].strip()
+        password = lines[i + 1].strip()
+        if username_input == user and password_input == password:
+            print("Login successful!")
+            return
 
-
-# If "set_pwd" is blank (yes), prints some stuff and asks if you want to "create a new account" with a Y or N question, and if "Y" or "y" then create a new pwd and set it for "set_pwd"
-if set_pwd == "":
-    print("===========================")
-    print("Do you want to create an account and set a new password?")
-    print("This will overwrite the current one! (for this version)")
-    time.sleep(1.5)
-    print("Options: \n Yes (Y) \n No (N)")
-
-    if input("Selected Option:") == "Y" or "y":
-        set_pwd = input("New Password: ")
-        login()
-    else:
-        exit()
-
-
-# Soooo... while the input password when trying to login isn't the same as the one you set earlier it will say "Wrong!" and keep calling login function
-while input_pwd != set_pwd:
-    print("Wrong!")
+    print("Incorrect username or password.")
+    time.sleep(1)
     login()
 
 
-# If the input password is the same as the one you set up earlier then print "Welcome back to your account!" and little silly prank (sorry)
-if input_pwd == set_pwd:
-    print("Welcome back to your account!")
-    webbrowser.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")              # I'm genuinely sorry, I'm disappointed in myself don't worry, I'll change it, someday.
+def create_account():
+    user = input("Input the desired username: ")
+    password = input("Enter the desired password: ")
+    with open("datos.txt", "a") as fa:  # "fa" = file append
+        fa.write(user + "\n")
+        fa.write(password + "\n")
+    print("Account created! You can login now.")
+    time.sleep(1)
+    login()
+
+
+with open("datos.txt", "r") as f:
+    contenido = f.read().strip()
+    if contenido == "":
+        print("No user found, want to create a new one?")
+        while True:
+            response = input("Yes (Y) or No (N): ").strip().lower()
+            if response == "y":
+                create_account()
+                break
+            elif response == "n":
+                login()
+                break
+            else:
+                print("Invalid option, try again.")
+    else:
+        login()
+
